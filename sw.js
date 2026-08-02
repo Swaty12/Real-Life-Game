789d3f7e1a46ebe1175de5a67e5d5a4f23de24fb
Swaty12 <46415553+Swaty12@users.noreply.github.com>
2026-08-02 13:18:28 +0300

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'rlg-v1';
+const ASSETS = ['./real-life-game.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
